bf3c38587f5b099fc43efe1288fb10b0f7e5cde1
unknown <vinicius_dasilvaalves@hotmail.com>
2021-01-09 16:31:31 -0300

    GET /products

---
+++ Backend/src/main/java/com/devsuperior/Dsdeliver/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.Dsdeliver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.Dsdeliver.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ Backend/src/main/java/com/devsuperior/Dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.Dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.Dsdeliver.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	List<Product> findAllByOrderByNameAsc();
+}
