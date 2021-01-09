b3fa1a90a045132996a17b812fa028a09319d364
unknown <vinicius_dasilvaalves@hotmail.com>
2021-01-09 17:10:53 -0300

    GET /orders

---
+++ Backend/src/main/java/com/devsuperior/Dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.Dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.Dsdeliver.dto.OrderDTO;
+import com.devsuperior.Dsdeliver.entities.Order;
+import com.devsuperior.Dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional (readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream() .map(x -> new OrderDTO(x)) .collect(Collectors.toList());
+	}
+}
